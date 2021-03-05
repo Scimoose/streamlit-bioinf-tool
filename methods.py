9ada5f52636f7973c6ce0d8aa10acce3a41e5070
Patryk Surmacz <patryksurmacz@gmail.com>
2021-03-05 12:24:27 +0100

    readability update
    main.py is now easier to read and update, most of the code is now in methods.py

--- methods.py
+++ methods.py
@@ -1,7 +1,9 @@
 from itertools import zip_longest
 from textwrap import wrap
+import streamlit as st
+import re
 
-
+# counts bases in a string and returns count + sum
 def count_bases(seq):
     A = seq.count("A")
     C = seq.count("C")
@@ -11,7 +13,7 @@ def count_bases(seq):
     nucleotide_sum = A + C + G + T
     return nucleotide_sum, A, C, G, T
 
-
+# translates RNA to protein
 def translate_to_prot(seq):
     rna_seq = seq.replace("T", "U")
     data = wrap(rna_seq, 3)
@@ -43,7 +45,51 @@ def translate_to_prot(seq):
 
     return prot_seq
 
+# mode function - basic DNA analysis
+def basic_DNA():
+    st.subheader("Analyze your DNA sequence:")
 
+    try:
+        uploaded_file = st.file_uploader("Upload Sequence", type="txt")
+        seq = uploaded_file.read()
+        seq = seq.decode('UTF-8')
+        st.text("Your sequence:")
+        st.text(seq)
+
+        sum, A, C, G, T = count_bases(seq)
+
+        st.write("There are: ", A, "A ", C, "C",
+                 G, "G", T, "T, in total ",
+                 sum, " nucleotides")
+        st.write("Which is ", round(A/sum*100, 3), "% A ",
+                 round(C/sum*100, 3), "% C",
+                 round(G/sum*100, 3), "% G",
+                 round(T/sum*100, 3), "% T")
+        st.write("Which means the GC content is ",
+                 round((G+C)/sum*100, 3), "%")
+
+        rna_switch = st.checkbox("1. Show RNA strand?")
+
+        if rna_switch is True:
+            st.text(seq.replace("T", "U"))
+            translate_to_prot = st.checkbox("1.1 Translate strand to protein?")
+
+            if translate_to_prot is True:
+
+                prot_seq = translate_to_prot(seq)
+
+                st.text(prot_seq)
+
+        comp_switch = st.checkbox("2. Show complementary DNA?")
+
+        if comp_switch is True:
+            st.text(seq.replace('A', 't').replace('T', 'a').replace('C', 'g')
+                    .replace('G', 'c').upper()[::-1])
+
+    except AttributeError:
+        pass
+
+# compares original and mutated string to find number of mutations
 def find_mutations(origin_seq, mut_seq):
     num_of_mut = 0
     for i in range(0, len(origin_seq)):
@@ -52,3 +98,55 @@ def find_mutations(origin_seq, mut_seq):
         else:
             num_of_mut += 1
     return num_of_mut
+
+# mode function - count point mutations
+def count_point_mut():
+    try:
+        st.subheader("Original sequence:")
+        origin_seq = st.file_uploader("Upload original sequence", type="txt")
+        origin_seq = origin_seq.read()
+        origin_seq = origin_seq.decode('UTF-8')
+        st.text(origin_seq)
+
+    except AttributeError:
+        pass
+
+    try:
+        st.write("Mutated sequence:")
+        mut_seq = st.file_uploader("Upload mutated sequence", type="txt")
+        mut_seq = mut_seq.read()
+        mut_seq = mut_seq.decode('UTF-8')
+        st.text(mut_seq)
+
+    except AttributeError:
+        pass
+
+    try:
+        num_of_mut = find_mutations(origin_seq, mut_seq)
+        st.write("There are ", num_of_mut, " point mutations.")
+    except TypeError:
+        pass
+
+# mode function - find motifs
+def find_motifs():
+    try:
+        st.subheader("Original sequence:")
+        check_motif_seq = st.file_uploader("Upload sequence that you want to find motifs in:", type="txt")
+        check_motif_seq = check_motif_seq.read()
+        check_motif_seq = check_motif_seq.decode('UTF-8')
+        st.text(check_motif_seq)
+
+        motif = st.text_input("Write motif sequence below...")
+
+        matches = re.finditer(motif, check_motif_seq)
+        positions = [match.start() for match in matches]
+
+        # arrays start at 0 so if I write a list directly it will look ugly
+        pos_dict = {}
+        for i, pos in enumerate(positions):
+            pos_dict[i+1] = pos + 1
+
+        st.write(pos_dict)
+
+    except AttributeError:
+        pass
